c6b4ed47d7b26835cbb2712d199a77d8b383cd54
Michael Stegeman <michael@stegeman.me>
2019-01-16 09:04:10 -0700

    Drop API level 1. (#15)
    Fixes #14

--- example-adapter.js
+++ example-adapter.js
@@ -8,21 +8,11 @@
 
 'use strict';
 
-let Adapter, Device, Property;
-try {
-  Adapter = require('../adapter');
-  Device = require('../device');
-  Property = require('../property');
-} catch (e) {
-  if (e.code !== 'MODULE_NOT_FOUND') {
-    throw e;
-  }
-
-  const gwa = require('gateway-addon');
-  Adapter = gwa.Adapter;
-  Device = gwa.Device;
-  Property = gwa.Property;
-}
+const {
+  Adapter,
+  Device,
+  Property,
+} = require('gateway-addon');
 
 class ExampleProperty extends Property {
   constructor(device, name, propertyDescription) {
